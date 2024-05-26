077aa99cfe7d1e2158259ae6ec7eacf3a48d9053
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-05-26 18:24:31 +0300

    HW2

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course_name = "Python"
+homework_done = 12
+expend_time = 1.5
+time_for_one_task = expend_time/homework_done
+print('Курс: ',course_name,', всего задач:',homework_done,', затрачено часов: ',expend_time,', среднее время выполнения',time_for_one_task,' часа.')
