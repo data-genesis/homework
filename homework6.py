6617d3e5eb9c1f5a05fb3c371832c9f251b052d6
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-05-27 14:12:42 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+my_dict = {'Rihana': 45, 'Bob': 86, 'Gretta': 68}
+print("Dict:", my_dict)
+print("Existing weight:", my_dict['Bob'])
+print("Not existing weight:",my_dict.get('Sara'))
+my_dict.update({'Hooper': 92,'Chad': 78})
+a = my_dict.pop('Rihana')
+print("Deleted value:", a)
+print("Updated dict:", my_dict)
+
+
+
+list = [3, 6, 7, 3, 'Yes', 6, 3,]
+my_set = set(list)
+list =
+
+print("Set:", set(my_set))
+print(my_set)
