1a453a0e5a1098022cbf651f657472e410368269
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-06-03 05:32:32 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,21 @@
+import random
+rocks = random.randint(3, 20)
+print(rocks)
+num1 = list(range(1, rocks + 1))
+num2 = list(range(1, rocks + 1))
+
+
+def function():
+    win_value = []
+    for n1 in num1:
+        if rocks % (n1 + n2) == 0:
+
+        for n2 in range(n1 + 1, n)
+            if rocks % (n1 + n2) == 0:
+                win_value.append(f"{n1}{n2}")
+            return win_value
+result = function()
+print(result)
+
+def function_first_field(rocks):
+    pass
