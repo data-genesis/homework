497a9096f668ace7cb0286bc55f5fe83e7a0eec7
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-07-30 14:39:08 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list, *functions):
+    result_dict = {}
+
+    for func in functions:
+        result_dict[func.__name__] = func(int_list)
+    return result_dict
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
