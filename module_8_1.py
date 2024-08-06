fd9d508c406d44f5fa6614e2e76531ad83de38bd
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-08-06 17:41:39 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,15 @@
+
+
+
+def add_everything_up(a, b):
+    try:
+        result = a + b
+        return round(result, 3)
+    except TypeError:
+        a = str(a)
+        b = str(b)
+        return f"{a}{b}"
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
