51d6d7054c775f5c074de6f187f344c257a3c7ac
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-08-15 23:03:42 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,11 @@
+def all_variants(text):
+    length = len(text)
+    for size in range(1, length + 1):
+        for start in range(length - size + 1):
+            yield text[start:start + size]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
