f08afc1f2b7d3df3706972720324740e1eb3f8ac
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-11-23 22:30:09 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,35 @@
+import random
+import unittest
+import logging
+from testing import Runner
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="runner_tests.log",
+    filemode="w",
+    encoding="utf-8",
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        try:
+            runner = Runner(name="Алекс", speed=-5)
+            runner.walk()
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.warning("Неверная скорость для Runner: %s", e)
+
+    def test_run(self):
+
+        try:
+            runner = Runner(name=123, speed=10)
+            runner.run()
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.warning("Неверный тип данных для объекта Runner: %s", e)
+
+if __name__ == "__main__":
+    unittest.main()
+
