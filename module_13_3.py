95d95dc9b9ad8a3a5a46cd12bc33a6eadc33820c
data-genesis <155980029+data-genesis@users.noreply.github.com>
2024-11-25 15:36:00 +0300

    Create module_13_3.py

---
+++ module_13_3.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+API_TOKEN = ""
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=["start"])
+async def start_message(message: types.Message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_message(message: types.Message):
+    print("Введите команду /start, чтобы начать общение.")
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
