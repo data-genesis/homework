1fc7d310991c52d8a650084fef8995f330fe5b01
data-genesis <155980029+data-genesis@users.noreply.github.com>
2025-01-25 15:34:47 +0300

    Update module10_1.py

--- module10_1.py
+++ module10_1.py
@@ -14,25 +14,39 @@ def write_words(word_count, file_name):
     time_res = time_end - time_start
     print(f"Завершилась запись в файл {file_name}")
 
+time_start1 = datetime.now()
+
 write_words(10, "example1.txt")
 write_words(30, "example2.txt")
 write_words(200, "example3.txt")
 write_words(100, "example4.txt")
 
-time_start2 = datetime.now()
+time_end1 = datetime.now()
+time_res1 = time_end1 - time_start1
+
+print(f"Работа функций {time_res1} секунд.")
+
+
+
 first = Thread(target=write_words, args=(10, "example5.txt"))
 second = Thread(target=write_words, args=(30, "example6.txt"))
 third = Thread(target=write_words, args=(200, "example7.txt"))
 fourth = Thread(target=write_words, args=(100, "example8.txt"))
+
+time_start2 = datetime.now()
 
 first.start()
 second.start()
 third.start()
 fourth.start()
 
+first.join()
+second.join()
+third.join()
+fourth.join()
+
 time_end2 = datetime.now()
 time_res2 = time_end2 - time_start2
 
-print(f"Потоки закончили работу за {time_res2} секунд.")
-
+print(f"Работа потоков {time_res2} секунд.")
 
